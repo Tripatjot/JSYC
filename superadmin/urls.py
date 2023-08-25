437dcfe2f46acbabb120b43b4b562be9d10e9a7c
TripatJot Singh <tripatjot.singh@indianpac.com>
2023-08-25 19:04:29 +0530

    6 API's

--- superadmin/urls.py
+++ superadmin/urls.py
@@ -1,18 +1,35 @@
 from django.urls import path 
-from .views import DistrictLisitng, LAU_Agents, TotalLeadsUnassignedbyLAUAdmin, TaskDistributionAPIView, BlockListing, TotalLeadAssignd, TotalLeadUnassigned, DataSourceListing, UploadingNewLeads, LauAssiginLeadsToAgents, AssignedLeadsforLAUAgents, ShowBasicInfotoLAUAgent, SaveChangesBasicInfo
+from .views import (DistrictLisitng, LAU_Agents, TotalLeadsUnassignedbyLAUAdmin, 
+                    TaskDistributionAPIView, BlockListing, TotalLeadAssignd, 
+                    TotalLeadUnassigned, DataSourceListing, UploadingNewLeads, 
+                    LauAssiginLeadsToAgents, AssignedLeadsforLAUAgents, ShowBasicInfotoLAUAgent, 
+                    SaveChangesBasicInfo, updateWAinfoByLAUAgent_1, updateWAinfoByLAUAgent_2,
+                    LauCallStatusListing, LauLeadStatusListing, LAUAdminConsenttoWA,
+                    LAUAdminWAtoSuperAdmin)
 
 urlpatterns = [
+    # SuperAdmin- New Leads
     path('get_district/', DistrictLisitng.as_view(), name = 'District'),
     path('get_block/', BlockListing.as_view(), name = 'Block'),
     path('get_datasource/', DataSourceListing.as_view(), name = 'Data Source'),
     path('uploadnewleads/', UploadingNewLeads.as_view(), name = 'Upload New Leads'),
     path('get_totalleads/', TotalLeadUnassigned.as_view(), name = 'Total Lead Unassigned'),
     path('get_totalleadassignd/',TotalLeadAssignd.as_view(), name='Total Lead Assignd'),
+    
+    # LAU Admin - Assign new Leads 
     path('get_lauagents/', LAU_Agents.as_view(), name = 'LAU Agents'),
     path('get_TotalLeadsUnassignedbyLAUAdmin/', TotalLeadsUnassignedbyLAUAdmin.as_view(), name = 'Total Leads Unassigned by LAU Admin'),
     path('get_lauAssiginLeadsToConsent/', LauAssiginLeadsToAgents.as_view(), name = 'Lau Assigin Leads To Consent'),
     path('get_TaskDistributionAPIView/', TaskDistributionAPIView.as_view(), name = 'Task Distribution API'),
+    
+    # LAU Agents - Dashboard
     path('get_AssignedLeadsforLAUAgents/', AssignedLeadsforLAUAgents.as_view(), name = 'Assigned Leads for LAU Agents'),
     path('get_ShowBasicInfotoLAUAgent/', ShowBasicInfotoLAUAgent.as_view(), name = 'Show Basic Info Lead to LAU Agent'),
     path('get_SaveChangesBasicInfo/', SaveChangesBasicInfo.as_view(), name = 'Save Changes Basic Info'),
+    path('get_updateWAinfoByLAUAgent_1/', updateWAinfoByLAUAgent_1.as_view(), name = 'update WA info By LAU Agent 1'),
+    path('get_updateWAinfoByLAUAgent_2/', updateWAinfoByLAUAgent_2.as_view(), name = 'Update WA info By LAU Agent_2'),
+    path('get_LauCallStatusListing/', LauCallStatusListing.as_view(), name = 'Lau Call Status Listing'),
+    path('get_LauLeadStatusListing/', LauLeadStatusListing.as_view(), name = 'Lau Lead Status Listing'),
+    path('get_LAUAdminConsenttoWA/', LAUAdminConsenttoWA.as_view(), name = 'LAU Admin Consent to WA'),
+    path('get_LAUAdminWAtoSuperAdmin/', LAUAdminWAtoSuperAdmin.as_view(), name = 'LAU Admin WA to Super Admin'),
 ]
--- superadmin/views.py
+++ superadmin/views.py
@@ -6,7 +6,6 @@ from users.models import User
 import pandas as pd
 from datetime import datetime
 import math
-from django.db import transaction
 from django.db.models import F,Q
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -319,13 +318,6 @@ class LauAssiginLeadsToAgents(APIView):
         elif get_dist and get_block and get_source:
             dist_block_source_data = all_data.filter(master__district=get_dist, master__block_ulb=get_block, master__master_source=get_source).values()
             lead_ids_for_assign = [dist_block_source_data[i]["id"] for i in range(get_leads_count)]
-
-        # split_consent_ids = list(map(int, consent_agent_ids.split(",")))
-        # final_list = [lead_ids_for_assign[i:i + len(split_consent_ids)] for i in range(0, len(lead_ids_for_assign), len(split_consent_ids))]
-
-        # for index, leads in enumerate(final_list):
-        #     if leads:
-        #         Registration.objects.filter(id__in=leads).update(lau_consent_agent_id=split_consent_ids[index])
         
         split_consent_ids = list(map(int, consent_agent_ids.split(",")))
         final_list = [lead_ids_for_assign[i:i + len(split_consent_ids)] for i in range(0, len(lead_ids_for_assign), len(split_consent_ids))]
@@ -438,6 +430,10 @@ class AssignedLeadsforLAUAgents(APIView):
         }
         try:
             lau_id = request.data['lau_id']
+            get_district = request.data['district']
+            get_block = request.data['block']
+            get_call_status = request.data['call_status']
+            get_lead_status = request.data['lead_status']
             
             # Retrieve relevant data for the given LAU agent ID
             data = Registration.objects.filter(
@@ -449,6 +445,30 @@ class AssignedLeadsforLAUAgents(APIView):
                 'lau_consent_followup_date_time', 'lau_consent_followup_date_time_2', 'lau_consent_followup_date_time_3'
             ).order_by('id')
             
+            if get_district:
+                data = data.filter(master__district = get_district)
+            if get_block:
+                data = data.filter(master__block_ulb = get_block)
+            if get_lead_status:
+                data = data.filter(lau_consent_lead_status = get_lead_status).exclude(
+                    Q(lau_consent_lead_status="All Criteria Fulfilled Consent") | 
+                    Q(lau_consent_lead_status="All Criteria Fulfilled WA") | 
+                    Q(lau_consent_lead_status="Not Interested") |
+                    Q(lau_consent_lead_status_2="All Criteria Fulfilled Consent") | 
+                    Q(lau_consent_lead_status_2="All Criteria Fulfilled WA") | 
+                    Q(lau_consent_lead_status_2="Not Interested") |
+                    Q(lau_consent_lead_status_3="All Criteria Fulfilled Consent") | 
+                    Q(lau_consent_lead_status_3="All Criteria Fulfilled WA") | 
+                    Q(lau_consent_lead_status_3="Not Interested"))
+            if get_call_status:
+                data = data.filter(lau_consent_lead_status = get_lead_status).exclude( 
+                    Q(lau_consent_call_status="Connected") | 
+                    Q(lau_consent_call_status="Wrong Number") |
+                    Q(lau_consent_call_status_2="Connected") |
+                    Q(lau_consent_call_status_2="Wrong Number") |
+                    Q(lau_consent_call_status_3="Connected") | 
+                    Q(lau_consent_call_status_3="Wrong Number"))
+                
             # Update the result dictionary
             result['status'] = "OK"
             result['valid'] = True
@@ -457,7 +477,7 @@ class AssignedLeadsforLAUAgents(APIView):
             
             return Response(result, status=status.HTTP_200_OK)
         except KeyError:
-            result['result']['message'] = "Missing or invalid input"
+            result['result']['message'] = "Missing or invalid `inp`ut"
             return Response(result, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             result['result']['message'] = str(e)
@@ -474,10 +494,8 @@ class ShowBasicInfotoLAUAgent(APIView):
             }
         }
         try:
-            # Retrieve lead ID from the request data
             lead_id = request.data['lead_id']
             
-            # Get registration data based on lead ID
             reg_id = Registration.objects.filter(id=lead_id).values()
             if not reg_id:
                 result['result']['message'] = "Registration data not found"
@@ -485,13 +503,11 @@ class ShowBasicInfotoLAUAgent(APIView):
             
             master_lead_id = reg_id[0]["master_id"]
             
-            # Get master data based on master_lead_id
             data = Master.objects.filter(id=master_lead_id).values()
             if not data:
                 result['result']['message'] = "Master data not found"
                 return Response(result, status=status.HTTP_404_NOT_FOUND)
             
-            # Update the result dictionary
             result['status'] = "OK"
             result['valid'] = True
             result['result']['message'] = "Data Fetched Successfully"
@@ -517,10 +533,10 @@ class SaveChangesBasicInfo(APIView):
         }
 
         try:
-            get_id = int(request.data.get('id'))
+            get_id = request.data.get('id')
             reg_instance = Registration.objects.get(id=get_id)
             master_lead_id = reg_instance.master_id
-
+            
             update_data = {
                 'sub_source_name': request.data.get('sub_source_name'),
                 'source_contact_no': request.data.get('source_contact_no'),
@@ -558,3 +574,260 @@ class SaveChangesBasicInfo(APIView):
             result['result']['error'] = str(e)
             return Response(result, status=status.HTTP_500_INTERNAL_SERVER_ERROR)  
       
+class updateWAinfoByLAUAgent_1(APIView):
+    def post(self, request):
+        result = {
+            'status': "NOK",
+            'valid': False,
+            'result': {
+                'message': "Data not Found",
+                'data': []
+            }
+        }
+        try: 
+            get_id = request.data['id']
+            
+            get_interested_in_opening_youth_club = request.data['interested_in_opening_youth_club']
+            get_founding_25_members = request.data['founding_25_members']
+            get_whatsapp_group_of_100 = request.data['whatsapp_group_of_100']
+            get_physical_meeting_space = request.data['physical_meeting_space']
+            get_ready_to_create_club_fb_page = request.data['ready_to_create_club_fb_page']
+            get_ready_pk_connect_app_download = request.data['ready_pk_connect_app_download']
+            
+            get_whatsapp_group_created = request.data['whatsapp_group_created']
+            get_wa_group_link = request.data['wa_group_link']
+            get_wa_group_name = request.data['wa_group_name']
+            get_wa_group_strength = request.data['wa_group_strength']
+            get_central_number_added = request.data['central_number_added']
+            get_pk_connect_app_download = request.data['pk_connect_app_download']
+            get_app_download_verified = request.data['app_download_verified']
+            
+            get_referral_name = request.data['referral_name']
+            get_referral_contact_no = request.data['referral_contact_no']
+            get_lau_remarks = request.data['lau_remarks']
+            
+            calling_mode = request.data['calling_mode']    
+            
+            if calling_mode == 'consent':
+                registration_data = {
+                    'interested_in_opening_youth_club': get_interested_in_opening_youth_club,
+                    'founding_25_members': get_founding_25_members,
+                    'whatsapp_group_of_100': get_whatsapp_group_of_100,
+                    'physical_meeting_space': get_physical_meeting_space,
+                    'ready_to_create_club_fb_page': get_ready_to_create_club_fb_page,
+                    'ready_to_pk_connect_app_download': get_ready_pk_connect_app_download,
+                    'whatsapp_group_created': get_whatsapp_group_created,
+                    'wa_group_link': get_wa_group_link,
+                    'wa_group_name': get_wa_group_name,
+                    'wa_group_strength': get_wa_group_strength,
+                    'central_number_added': get_central_number_added,
+                    'pk_connect_app_download': get_pk_connect_app_download,
+                    'app_download_verified': get_app_download_verified,
+                    'referral_name': get_referral_name,
+                    'referral_contact_no': get_referral_contact_no,
+                    'lau_remarks': get_lau_remarks,
+                    'lau_last_updated_date_time': datetime.today(),
+                }
+                print(registration_data)
+                Registration.objects.filter(id=get_id).update(**registration_data)
+            elif calling_mode == 'whatsapp' :
+                registration_data = {
+                    'interested_in_opening_youth_club': get_interested_in_opening_youth_club,
+                    'founding_25_members': get_founding_25_members,
+                    'whatsapp_group_of_100': get_whatsapp_group_of_100,
+                    'physical_meeting_space': get_physical_meeting_space,
+                    'ready_to_create_club_fb_page': get_ready_to_create_club_fb_page,
+                    'ready_to_pk_connect_app_download': get_ready_pk_connect_app_download,
+                    'whatsapp_group_created': get_whatsapp_group_created,
+                    'wa_group_link': get_wa_group_link,
+                    'wa_group_name': get_wa_group_name,
+                    'wa_group_strength': get_wa_group_strength,
+                    'central_number_added': get_central_number_added,
+                    'pk_connect_app_download': get_pk_connect_app_download,
+                    'app_download_verified': get_app_download_verified,
+                    'referral_name': get_referral_name,
+                    'referral_contact_no': get_referral_contact_no,
+                    'lau_remarks': get_lau_remarks,
+                    'lau_last_updated_date_time': datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
+                }
+                
+                print(registration_data)
+                Registration.objects.filter(id=get_id).update(**registration_data)
+            else:
+                result['result']['message'] = "Invalid calling_mode"
+                return Response(result, status=status.HTTP_400_BAD_REQUEST)
+            
+            reg_instance = Registration.objects.get (id= get_id)
+            master_lead_id = reg_instance.master_id
+            
+            data = Master.objects.filter(id= master_lead_id).values()
+            data_1 = Registration.objects.filter(id= get_id).values()
+            
+            result['status'] = "OK"
+            result['valid'] = True
+            result['result']['message'] = "Data updated successfully"
+            result['result']['data'] = list(data), list(data_1)
+            return Response(result, status=status.HTTP_200_OK)
+        except KeyError as e:
+            result['result']['message'] = f"KeyError: {str(e)} not found in request data"
+            return Response(result, status=status.HTTP_400_BAD_REQUEST)
+        except ObjectDoesNotExist:
+            result['result']['message'] = "Object not found"
+            return Response(result, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            result['result']['message'] = f"An error occurred: {str(e)}"
+            return Response(result, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class updateWAinfoByLAUAgent_2(APIView):
+    def post (self, request):
+        result = {
+            'status': "NOK",
+            'valid': False,
+            'result': {
+                'message': "Data not Found",
+                'data': []
+            }
+        }
+        
+        try:
+            get_id= request.data['id']
+            get_call_status = request.data['call_status']
+            get_lead_status = request.data['lead_status']
+            get_follow_date_time = request.data['follow_date_time']
+            get_remarks = request.data['remarks']
+            
+            registration_data = {
+                'lau_consent_call_status' : get_call_status,
+                'lau_consent_lead_status' : get_lead_status,
+                'lau_last_updated_date_time': datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
+                'lau_consent_remarks' : get_remarks
+            }
+            Registration.objects.filter(id=get_id).update(**registration_data)
+            data = Registration.objects.filter(id=get_id).values()
+            result['status'] = "OK"
+            result['valid'] = True
+            result['result']['message'] = "Data updated successfully"
+            result['result']['data'] = list(data)
+            return Response(result, status=status.HTTP_200_OK)
+        except KeyError as e:
+            result['result']['message'] = f"KeyError: {str(e)} not found in request data"
+            return Response(result, status=status.HTTP_400_BAD_REQUEST)
+        except ObjectDoesNotExist:
+            result['result']['message'] = "Object not found"
+            return Response(result, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            result['result']['message'] = f"An error occurred: {str(e)}"
+            return Response(result, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class LauCallStatusListing(APIView):
+    def get(self, request):
+        result = {
+            'status': "NOK",
+            'valid': False,
+            'result': {
+                'message': "Data not Found",
+                'data': []
+            }
+        }
+        try:
+            call_status = ["Connected", "Call Back Later", "Busy", 
+                           "Call Not answered", "Switched off",
+                           "Number not reachable", "Wrong Number"]
+            result['status'] = "OK"
+            result['valid'] = True
+            result['result']['message'] = "Data retrieved successfully"
+            result['result']['data'] = call_status 
+            return Response(result, status=status.HTTP_200_OK)
+        except Exception as e:
+            result['result']['message'] = f"An error occurred: {str(e)}"
+            return Response(result, status=status.HTTP_500_INTERNAL_SERVER_ERROR)      
+        
+class LauLeadStatusListing(APIView):
+    def get(self, request):
+        result = {
+            'status': "NOK",
+            'valid': False,
+            'result': {
+                'message': "Data not Found",
+                'data': []
+            }
+        }
+        try:
+            call_status = ["All Criteria Fulfilled Consent", "All Criteria Fulfilled WA", 
+                           "Not Interested", "Followup"]
+            result['status'] = "OK"
+            result['valid'] = True
+            result['result']['message'] = "Data retrieved successfully"
+            result['result']['data'] = call_status 
+            return Response(result, status=status.HTTP_200_OK)
+        except Exception as e:
+            result['result']['message'] = f"An error occurred: {str(e)}"
+            return Response(result, status=status.HTTP_500_INTERNAL_SERVER_ERROR) 
+        
+# LAU Admin - Assign Leads-Consent      
+class LAUAdminConsenttoWA(APIView):
+    def post(self, request):
+        result = {
+            'status': "NOK",
+            'valid': False,
+            'result': {
+                'message': "Data not Found",
+                'data': []
+            }
+        }
+        get_district = request.data.get('district')
+        get_block = request.data.get('block')
+        get_source = request.data.get('source')
+        
+        data = Registration.objects.all().filter(lau_consent_agent_id__isnull = False)
+        if get_district:
+            data = data.filter(master__district = get_district)
+        if get_block:
+            data = data.filter(master__block_ulb = get_block)
+        if get_source:
+            data = data.filter(master__master_source = get_source)
+        
+        
+        result['status'] = "OK"
+        result['valid'] = True
+        result['result']['message'] = "Data retrieved successfully"
+        result['result']['data'] = data.values(
+                'id', 'master__uid', 'master__name', 
+                'master__district', 'master__block_ulb', 
+                'master__master_source','lau_consent_lead_status',
+            )
+        return Response(result, status=status.HTTP_200_OK) 
+        
+class LAUAdminWAtoSuperAdmin(APIView):
+    def post(self, request):
+        result = {
+            'status': "NOK",
+            'valid': False,
+            'result': {
+                'message': "Data not Found",
+                'data': []
+            }
+        }
+        get_district = request.data.get('district')
+        get_block = request.data.get('block')
+        get_source = request.data.get('source')
+        
+        data = Registration.objects.all().filter(lau_consent_agent_id__isnull = False).exclude(Q (lau_lead_status = "All Criteria Fulfilled Consent") | Q (lau_lead_status__isnull = False))
+        if get_district:
+            data = data.filter(master__district = get_district)
+        if get_block:
+            data = data.filter(master__block_ulb = get_block)
+        if get_source:
+            data = data.filter(master__master_source = get_source)
+        
+        
+        result['status'] = "OK"
+        result['valid'] = True
+        result['result']['message'] = "Data retrieved successfully"
+        result['result']['data'] = data.values(
+                'id', 'master__uid', 'master__name', 
+                'master__district', 'master__block_ulb', 
+                'master__master_source','lau_lead_status','wa_group_strength'
+            )
+        return Response(result, status=status.HTTP_200_OK) 
+        
